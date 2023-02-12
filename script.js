3038d7e711468bf84017b653f54a7fda462083ac
Dillon Scott <dillonscott.2024@gmail.com>
2023-02-11 19:41:14 -0500

    footer

--- script.js
+++ script.js
@@ -2,10 +2,18 @@
 
 document.getElementById("preferences-button").addEventListener("click", () => {
   document.getElementById("statistics").classList.remove("visible");
+  document
+    .getElementById("statistics-button")
+    .classList.remove("active-button");
   document.getElementById("preferences").classList.add("visible");
+  document.getElementById("preferences-button").classList.add("active-button");
 });
 
 document.getElementById("statistics-button").addEventListener("click", () => {
-  document.getElementById("preferences").classList.remove("visible");
   document.getElementById("statistics").classList.add("visible");
+  document.getElementById("statistics-button").classList.add("active-button");
+  document.getElementById("preferences").classList.remove("visible");
+  document
+    .getElementById("preferences-button")
+    .classList.remove("active-button");
 });
